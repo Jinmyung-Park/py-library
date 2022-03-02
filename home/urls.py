0ed454cf8b94b990983a057b2a472864d2016fe1
park <ckaskdhs@naver.com>
2022-03-02 15:24:36 +0900

    modify gitignore

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('',auth_views.LoginView.as_view(template_name='layouts/app.html'),name='login'),
+    path('',auth_views.LoginView.as_view(template_name='title.html'),name='login'),
     path('home', views.home,name='home'),
 ]
 
